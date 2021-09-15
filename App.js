faf05bbcfe2efe0fa5046cc8798fbec818cd5655
PABourdais <pierre.alex.bourdais@gmail.com>
2021-09-15 16:33:42 +0200

    Creating the game world

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GameEngine
+        style={styles.gameEngine}
+      >
+      </GameEngine>
+      <StatusBar style="auto" hidden={true}/>
     </View>
   );
 }
@@ -14,8 +18,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  gameEngine: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
 });
