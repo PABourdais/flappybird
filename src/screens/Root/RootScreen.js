72adccd747a0e44c2eed6b1d2961dee3f5e85757
PABourdais <pierre.alex.bourdais@gmail.com>
2021-09-19 18:21:43 +0200

    Modify rootScreen

--- src/screens/Root/RootScreen.js
+++ src/screens/Root/RootScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, StatusBar } from 'react-native';
 
 const RootScreen = () =>  {
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity style= {{backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10}}
+            <StatusBar style="auto" hidden={true}/>
+            <TouchableOpacity style= {styles.button}
                               onPress={()=> {
                                console.log('hi')
                               }}>
@@ -22,5 +23,15 @@ export default RootScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
+    button: {
+        alignItems: 'center',
+        backgroundColor: 'black',
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+        width: '70%'
+    }
 });
